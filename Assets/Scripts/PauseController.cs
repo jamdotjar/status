da8b6622c7597987732d2c0c010f3a225aa5f705
jam.jar <finnhofbauer13@icloud.com>
2023-05-20 16:22:50 -0700

    level select technically works, pausing on the other hand

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseMenu; // Assign the pause menu panel in the editor
+    private bool isPaused = false;
+    void Start(){
+        pauseMenu.SetActive(isPaused);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        isPaused = !isPaused;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
+    public void ResumeGame()
+    {
+        TogglePauseMenu();
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu"); // Replace with the name of your main menu scene
+    }
+}
